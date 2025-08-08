2d0e2d60c8d5632f20a760c8239cf1d7b1f73e85
ysawoo <144856762+ysawoo@users.noreply.github.com>
2025-08-08 15:36:12 -0300

    subo el proyecto react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const nombre= "Ysabel Garzón"
+  const edad= 20;
+  const ocupación="estudiante"
+  return (
+    <div className="App">
+      <h1>Hola mi nombre es{nombre}</h1>
+        <h2>Tengo{edad}años</h2>
+        <p>Mi profesion es{ocupación}</p>
+      
+    </div>
+  );
+}
+export default App;
